ce2bcfef5e59889b34c4760556ddbf3bb62f2953
RaflyR334 <muhammadraflyrasyidin@gmail.com>
2024-07-26 14:22:42 +0700

    ubah

--- app/Models/aktor.php
+++ app/Models/aktor.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class aktor extends Model
+class Aktor extends Model
 {
     use HasFactory;
 
