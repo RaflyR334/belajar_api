7c39261b3ef0fdb3cca40223a988973932710ce5
RaflyR334 <muhammadraflyrasyidin@gmail.com>
2024-08-07 09:55:25 +0700

    ubahhhh

--- routes/api.php
+++ routes/api.php
@@ -54,5 +54,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
 });
 
 // auth route
+Route::post('logout', [AuthController::class, 'logout']);
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
